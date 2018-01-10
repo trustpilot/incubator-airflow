2a0e07003c7fb5230ef5f08a61d71e4a94cf940f
Gediminas Guoba <ggu@trustpilot.com>
2018-01-10 15:46:54 +0200

    Updated gsp_dataflow_hook from master

--- airflow/contrib/hooks/gcp_dataflow_hook.py
+++ airflow/contrib/hooks/gcp_dataflow_hook.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import json
 import select
 import subprocess
 import time
@@ -23,12 +24,13 @@ from airflow.utils.log.logging_mixin import LoggingMixin
 
 
 class _DataflowJob(LoggingMixin):
-    def __init__(self, dataflow, project_number, name):
+    def __init__(self, dataflow, project_number, name, poll_sleep=10):
         self._dataflow = dataflow
         self._project_number = project_number
         self._job_name = name
         self._job_id = None
         self._job = self._get_job()
+        self._poll_sleep = poll_sleep
 
     def _get_job_id_from_name(self):
         jobs = self._dataflow.projects().jobs().list(
@@ -70,7 +72,7 @@ class _DataflowJob(LoggingMixin):
                     raise Exception("Google Cloud Dataflow job {} was cancelled.".format(
                         self._job['name']))
                 elif 'JOB_STATE_RUNNING' == self._job['currentState']:
-                    time.sleep(10)
+                    time.sleep(self._poll_sleep)
                 elif 'JOB_STATE_PENDING' == self._job['currentState']:
                     time.sleep(15)
                 else:
@@ -89,12 +91,20 @@ class _DataflowJob(LoggingMixin):
 
 class _Dataflow(LoggingMixin):
     def __init__(self, cmd):
-        self._proc = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE,
-                                      stderr=subprocess.PIPE)
+        self.log.info("Running command: %s", ' '.join(cmd))
+        self._proc = subprocess.Popen(
+            cmd,
+            shell=False,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            close_fds=True)
 
     def _line(self, fd):
         if fd == self._proc.stderr.fileno():
-            line = self._proc.stderr.readline()
+            lines = self._proc.stderr.readlines()
+            for line in lines:
+              self.log.warning(line[:-1])
+            line = lines[-1][:-1]
             return line
         if fd == self._proc.stdout.fileno():
             line = self._proc.stdout.readline()
@@ -126,7 +136,9 @@ class DataFlowHook(GoogleCloudBaseHook):
 
     def __init__(self,
                  gcp_conn_id='google_cloud_default',
-                 delegate_to=None):
+                 delegate_to=None,
+                 poll_sleep=10):
+        self.poll_sleep = poll_sleep
         super(DataFlowHook, self).__init__(gcp_conn_id, delegate_to)
 
     def get_conn(self):
@@ -136,17 +148,23 @@ class DataFlowHook(GoogleCloudBaseHook):
         http_authorized = self._authorize()
         return build('dataflow', 'v1b3', http=http_authorized)
 
-    def _start_dataflow(self, task_id, variables, dataflow, name, command_prefix):
-        cmd = command_prefix + self._build_cmd(task_id, variables, dataflow)
+    def _start_dataflow(self, task_id, variables, dataflow,
+                        name, command_prefix, label_formatter):
+        cmd = command_prefix + self._build_cmd(task_id, variables,
+                                               dataflow, label_formatter)
         _Dataflow(cmd).wait_for_done()
-        _DataflowJob(
-            self.get_conn(), variables['project'], name).wait_for_done()
+        _DataflowJob(self.get_conn(), variables['project'],
+                     name, self.poll_sleep).wait_for_done()
 
     def start_java_dataflow(self, task_id, variables, dataflow):
         name = task_id + "-" + str(uuid.uuid1())[:8]
         variables['jobName'] = name
-        self._start_dataflow(
-            task_id, variables, dataflow, name, ["java", "-jar"])
+
+        def label_formatter(labels_dict):
+            return ['--labels={}'.format(
+                    json.dumps(labels_dict).replace(' ', ''))]
+        self._start_dataflow(task_id, variables, dataflow, name,
+                             ["java", "-jar"], label_formatter)
 
     def start_template_dataflow(self, task_id, variables, parameters, dataflow_template):
         name = task_id + "-" + str(uuid.uuid1())[:8]
@@ -156,14 +174,21 @@ class DataFlowHook(GoogleCloudBaseHook):
     def start_python_dataflow(self, task_id, variables, dataflow, py_options):
         name = task_id + "-" + str(uuid.uuid1())[:8]
         variables["job_name"] = name
-        self._start_dataflow(
-            task_id, variables, dataflow, name, ["python"] + py_options)
 
-    def _build_cmd(self, task_id, variables, dataflow):
+        def label_formatter(labels_dict):
+            return ['--labels={}={}'.format(key, value)
+                    for key, value in labels_dict.items()]
+        self._start_dataflow(task_id, variables, dataflow, name,
+                             ["python"] + py_options, label_formatter)
+
+    def _build_cmd(self, task_id, variables, dataflow, label_formatter):
         command = [dataflow, "--runner=DataflowRunner"]
         if variables is not None:
-            for attr, value in variables.iteritems():
-                command.append("--" + attr + "=" + value)
+            for attr, value in variables.items():
+                if attr == 'labels':
+                    command += label_formatter(value)
+                else:
+                    command.append("--" + attr + "=" + value)
         return command
 
     def _start_template_dataflow(self, name, variables, parameters, dataflow_template):
