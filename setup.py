3af5212707968c1af918d84dd234a860e35697f0
ebartkus <eba@trustpilot.com>
2018-09-17 11:33:00 +0300

    Async is reserved word in python 3.7

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ def write_version(filename=os.path.join(*['airflow',
     with open(filename, 'w') as a:
         a.write(text)
 
-async = [
+asynchronous = [
     'greenlet>=0.4.9',
     'eventlet>= 0.9.7',
     'gevent>=0.13'
@@ -246,7 +246,7 @@ def do_setup():
         extras_require={
             'all': devel_all,
             'all_dbs': all_dbs,
-            'async': async,
+            'async': asynchronous,
             'azure': azure,
             'celery': celery,
             'cgroups': cgroups,
